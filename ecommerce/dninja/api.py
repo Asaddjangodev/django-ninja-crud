fa814a240753fa9bc443cdffcf72b8960b110d8c
Asadbek <hoshimovaustin0550@gmail.com>
2025-08-04 06:05:11 +0900

    Add single and multiple media upload endpoint

--- ecommerce/dninja/api.py
+++ ecommerce/dninja/api.py
@@ -1,11 +1,11 @@
 from http.client import responses
 
-from ninja import NinjaAPI, Form
+from ninja import NinjaAPI, Form, File
+from ninja.files import UploadedFile
 from .schema import CategorySchema, ProductSchema
-from ecommerce.inventory.models import Category, Product
+from ecommerce.inventory.models import Category, Product, Media
 from typing import List
 from django.shortcuts import get_object_or_404
-
 
 api = NinjaAPI()
 
@@ -54,3 +54,22 @@ def delete_category(request, cat_id: int):
 def post_category_form(request, form: CategorySchema=Form(...)):
     category = Category.objects.create(**form.dict())
     return {"name": category.name}
+
+@api.post("inventory/category/form/params")
+def post_category_form_params(request, name:str=Form(...), slug:str=Form(...)):
+    category = Category.objects.create(name=name, slug=slug)
+    return {"name": category.name}
+
+# need to be committed
+@api.post("/upload/media")
+def upload_media(request, prod_id: int=Form(...), file: UploadedFile=File(...)):
+    product = get_object_or_404(Product, id=prod_id)
+    media = Media.objects.create(product_inventory=product, img_url=file)
+    return {"id": media.id}
+# need to be committed
+@api.post("/upload/media/multiple")
+def upload_media_multiple(request, prod_id: int=Form(...), files: List[UploadedFile]=File(...) ):
+    product = get_object_or_404(Product, id=prod_id)
+    for img in files:
+        Media.objects.create(product_inventory=product, img_url=img)
+    return {"Success": True}
